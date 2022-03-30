de131766be1f90cdd6d45e2538daee0352591b7f
mahdi nemati <this.nemati@gmail.com>
2022-03-30 12:37:16 +0430

    develope style of Article component

--- src/Components/ArticleComponent/Article.jsx
+++ src/Components/ArticleComponent/Article.jsx
@@ -14,11 +14,15 @@ const Article = () => {
   if (loading) return <SimpleBackdrop />;
   if (error) return <p>something went wrong!</p>;
   return (
-    <section>
-      <h1>{Article.title}</h1>
-      <p>{Article.author}</p>
-      <p>{Article.body}</p>
-    </section>
+    <main className="flex justify-center mt-8">
+      <section className="bg-white bg-opacity-40 w-11/12 rounded-2xl flex flex-col justify-center items-center p-5">
+        <h1 className="text-5xl mb-4">{Article.title}</h1>
+        <p className="text-xl mb-12">{Article.author}</p>
+        <p className="border border-gray-300 w-9/12 flex justify-start p-4">
+          {Article.body}
+        </p>
+      </section>
+    </main>
   );
 };
 
