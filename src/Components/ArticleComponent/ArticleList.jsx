65a29bae65d14085a080a9b1409a4c197b076991
mahdi nemati <this.nemati@gmail.com>
2022-03-30 09:32:23 +0430

    develope ArticleList component style

--- src/Components/ArticleComponent/ArticleList.jsx
+++ src/Components/ArticleComponent/ArticleList.jsx
@@ -11,17 +11,19 @@ export default function ArticleList() {
   if (loading) return <Loading />;
   if (error) return <p>something went wrong!</p>;
   return (
-    <main className="w-full flex flex-col justify-center items-center">
-      <h1>Article List</h1>
+    <main className="w-full flex flex-col justify-center items-center mt-5">
+      <h1 className="mb-4 text-4xl">Article List</h1>
       <section className="mt-4 mb-2 bg-white bg-opacity-40 w-11/12 rounded-2xl">
-        <div className="flex">
-          <div>Title</div>
-          <div>Author</div>
+        <div className="flex bg-slate-800 text-white h-10 text-2xl rounded-t-2xl">
+          <div className="flex justify-between w-2/5 pl-4">
+            <div>Title</div>
+            <div>Author</div>
+          </div>
         </div>
         {Article ? (
           Article.map((a) => {
             return (
-              <div className="flex">
+              <div className="flex justify-between w-5/12 pl-5 pt-2 pb-2 mb-2 mt-1 hover:bg-gray-300 cursor-pointer">
                 <p>{a.title}</p>
                 <p>{a.author}</p>
               </div>
